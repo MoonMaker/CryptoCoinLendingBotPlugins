1263f6a6b09e66aa4fcaedcf67b91ee90d0df0a1
MoonMaker <1302904+MoonMaker@users.noreply.github.com>
2017-10-25 16:01:21 +0200

    Updated Install Script

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-DIR=$(dirname "${VAR}")
+# To store the path of the executing script's directory in var `__DIR__`:
+DIR="$(cd "$(dirname "${0}")"; echo "$(pwd)")"
 
 ln -s ${DIR}/plugins/analysecharts/AnalysisCharts.py ${DIR}/plugins/AnalysisCharts.py
 ln -s ${DIR}/plugins/analysecharts/www/analysischarts ${DIR}/www/analysischarts
