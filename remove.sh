b8120e684e79f4b9e6603a92028357e8b40f2e79
MoonMaker <1302904+MoonMaker@users.noreply.github.com>
2017-10-25 16:05:41 +0200

    Changed remove script

--- remove.sh
+++ remove.sh
@@ -3,7 +3,7 @@
 # To store the path of the executing script's directory in var `__DIR__`:
 DIR="$(cd "$(dirname "${0}")"; echo "$(pwd)")"
 
-ln -s ${DIR}/plugins/analysecharts/AnalysisCharts.py ${DIR}/plugins/AnalysisCharts.py
-ln -s ${DIR}/plugins/analysecharts/www/analysischarts ${DIR}/www/analysischarts
-ln -s ${DIR}/plugins/analysecharts/www/analysischarts.html ${DIR}/www/analysischarts.html
+rm ${DIR}/plugins/AnalysisCharts.py
+rm ${DIR}/www/analysischarts
+rm ${DIR}/www/analysischarts.html
 
