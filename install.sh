43fb922c5857af1ac0c63d87afa32f9fa7372fd9
MoonMaker <1302904+MoonMaker@users.noreply.github.com>
2017-10-25 17:28:42 +0200

    .

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 # To store the path of the executing script's directory in var `__DIR__`:
 DIR="$(cd "$(dirname "${0}")"; echo "$(pwd)")"
 
-ln -s ${DIR}/AnalysisCharts.py ../AnalysisCharts.py
+ln -s ${DIR}/AnalysisCharts.py ${DIR}/../AnalysisCharts.py
 ln -s ${DIR}/www/analysischarts ${DIR}/../../www/analysischarts
 ln -s ${DIR}/www/analysischarts.html ${DIR}/../../www/analysischarts.html
