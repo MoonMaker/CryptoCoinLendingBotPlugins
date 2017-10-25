e97f98fb539a95de7bba8d12ff30ded33683201f
MoonMaker <1302904+MoonMaker@users.noreply.github.com>
2017-10-25 16:38:07 +0200

    Updated install & remove scripts

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 # To store the path of the executing script's directory in var `__DIR__`:
 DIR="$(cd "$(dirname "${0}")"; echo "$(pwd)")"
 
-ln -s ${DIR}/plugins/analysecharts/AnalysisCharts.py ${DIR}/plugins/AnalysisCharts.py
-ln -s ${DIR}/plugins/analysecharts/www/analysischarts ${DIR}/www/analysischarts
-ln -s ${DIR}/plugins/analysecharts/www/analysischarts.html ${DIR}/www/analysischarts.html
-
+ln -s ${DIR}/AnalysisCharts.py ../AnalysisCharts.py
+ln -s ${DIR}/www/analysischarts ${DIR}/../../www/analysischarts
+ln -s ${DIR}/www/analysischarts.html ${DIR}/../../www/analysischarts.html
--- remove.sh
+++ remove.sh
@@ -3,7 +3,6 @@
 # To store the path of the executing script's directory in var `__DIR__`:
 DIR="$(cd "$(dirname "${0}")"; echo "$(pwd)")"
 
-rm ${DIR}/plugins/AnalysisCharts.py
-rm ${DIR}/www/analysischarts
-rm ${DIR}/www/analysischarts.html
-
+rm ${DIR}/../AnalysisCharts.py
+rm ${DIR}/../../www/analysischarts
+rm ${DIR}/../../www/analysischarts.html
